4e384f7e60f713d2355172fdfe04b5838f44b57d
Jose Augusto Costa <75041668+JoseAugustoDev@users.noreply.github.com>
2025-09-09 18:55:30 -0300

    fix: Atualizando dependencias e ajustando teste

--- cypress/e2e/nav.cy.ts
+++ cypress/e2e/nav.cy.ts
@@ -1,7 +1,7 @@
 describe('navigation', () => {
   it('Verificando se a navegacao esta funcionando', () => {
     cy.visit('http://localhost:3000')
-    cy.get('[data-test="course-0"]').click()
+    cy.contains('Get started').click()
     cy.url().should('include', '/testing-your-first-application')
   })
 })
