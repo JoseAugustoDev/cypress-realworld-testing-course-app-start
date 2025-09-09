292b8a58327246232e463fe3367f94398e2f8bc0
Jose Augusto Costa <75041668+JoseAugustoDev@users.noreply.github.com>
2025-09-09 18:59:34 -0300

    fix: Eliminando teste e consertando outro

--- cypress/e2e/form.cy.ts
+++ cypress/e2e/form.cy.ts
@@ -2,5 +2,6 @@ describe('email input', () => {
   it('Verificando o tipo do email digitado', () => {
     cy.visit('http://localhost:3000')
     cy.get('#email-address').type('jose@sigex.com')
+    cy.get('.mt-4 > .mt-3 > .w-full').click()
   })
 })
